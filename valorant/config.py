559ba47657278161732ddb7a0848060adc09c2b3
Jean Castro <jeancastrogudiel@gmail.com>
2025-11-05 16:37:28 -0600

    Update config.py

--- valorant/config.py
+++ valorant/config.py
@@ -1,9 +1,7 @@
 import os
 
-# Configuración del jugador y API
 NOMBRE = "Ponssloveless"
 TAG    = "8882"
 REGION = "na"
 
-# API key de HenrikDev: se recomienda setearla como variable de entorno
 API_KEY = os.environ.get("API_KEY", "")
