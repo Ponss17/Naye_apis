5f1385c92feceb3a7290c7d3cb2702da655591c3
Jean Castro <jeancastrogudiel@gmail.com>
2025-11-07 11:06:43 -0600

    Update config.py

--- twitch/config.py
+++ twitch/config.py
@@ -1,5 +1,5 @@
 import os
 
-CLIENT_ID = os.environ.get("TWITCH_CLIENT_ID", "")
-CLIENT_SECRET = os.environ.get("TWITCH_CLIENT_SECRET", "")
-CHANNEL_LOGIN = os.environ.get("TWITCH_CHANNEL_LOGIN", "")
+CLIENT_ID = os.environ.get("TWITCH_CLIENT_ID") or os.environ.get("CLIENT_ID", "")
+CLIENT_SECRET = os.environ.get("TWITCH_CLIENT_SECRET") or os.environ.get("CLIENT_SECRET", "")
+CHANNEL_LOGIN = os.environ.get("TWITCH_CHANNEL_LOGIN") or os.environ.get("CHANNEL_LOGIN", "")
